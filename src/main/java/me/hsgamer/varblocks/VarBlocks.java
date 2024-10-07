ae10dacd38a83336224178cbd6cb89b838e9baad
hsgamer <huynhqtienvtag@gmail.com>
2024-10-08 00:58:49 +0700

    Initial commit

---
+++ src/main/java/me/hsgamer/varblocks/VarBlocks.java
@@ -0,0 +1,17 @@
+package me.hsgamer.varblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VarBlocks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
