7f6dac62f9c131a8d726839c5f5680c45ddd9893
hsgamer <huynhqtienvtag@gmail.com>
2024-10-19 00:25:09 +0700

    implement TemplateManager

--- src/main/java/me/hsgamer/varblocks/VarBlocks.java
+++ src/main/java/me/hsgamer/varblocks/VarBlocks.java
@@ -1,6 +1,32 @@
 package me.hsgamer.varblocks;
 
 import io.github.projectunified.minelib.plugin.base.BasePlugin;
+import me.hsgamer.hscore.bukkit.variable.BukkitVariableBundle;
+import me.hsgamer.hscore.variable.VariableBundle;
+import me.hsgamer.varblocks.manager.BlockUpdaterManager;
+import me.hsgamer.varblocks.manager.TemplateManager;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class VarBlocks extends BasePlugin {
+    private final VariableBundle variableBundle = new VariableBundle();
+
+    @Override
+    protected List<Object> getComponents() {
+        return Arrays.asList(
+                new TemplateManager(this),
+                new BlockUpdaterManager()
+        );
+    }
+
+    @Override
+    public void load() {
+        BukkitVariableBundle.registerVariables(variableBundle);
+    }
+
+    @Override
+    public void disable() {
+        variableBundle.unregisterAll();
+    }
 }
---
+++ src/main/java/me/hsgamer/varblocks/manager/TemplateManager.java
@@ -0,0 +1,43 @@
+package me.hsgamer.varblocks.manager;
+
+import me.hsgamer.hscore.bukkit.config.BukkitConfig;
+import me.hsgamer.hscore.common.CollectionUtils;
+import me.hsgamer.hscore.config.PathString;
+import me.hsgamer.hscore.variable.VariableManager;
+import org.bukkit.plugin.Plugin;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TemplateManager {
+    private final BukkitConfig config;
+
+    public TemplateManager(Plugin plugin) {
+        this.config = new BukkitConfig(plugin, "templates.yml");
+        this.config.setup();
+    }
+
+    public List<String> getTemplate(String name) {
+        return CollectionUtils.createStringListFromObject(config.getNormalized(name), false);
+    }
+
+    public void saveTemplate(String name, List<String> template) {
+        config.set(template, name);
+        config.save();
+    }
+
+    public List<String> getTemplateNames() {
+        return config.getKeys(false).stream().map(PathString::joinDefault).collect(Collectors.toList());
+    }
+
+    public List<String> getParsedTemplate(String name, Map<String, String> args) {
+        return getTemplate(name).stream().map(s -> {
+            for (Map.Entry<String, String> entry : args.entrySet()) {
+                s = s.replace("{" + entry.getKey() + "}", entry.getValue());
+            }
+            s = VariableManager.GLOBAL.replaceOrOriginal(s, null);
+            return s;
+        }).collect(Collectors.toList());
+    }
+}
