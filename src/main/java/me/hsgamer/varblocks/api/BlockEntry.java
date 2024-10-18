950e940fbd1485d6d9faaeac7a8df2af8068bb05
hsgamer <huynhqtienvtag@gmail.com>
2024-10-19 00:50:32 +0700

    add template name to BlockEntry

--- src/main/java/me/hsgamer/varblocks/api/BlockEntry.java
+++ src/main/java/me/hsgamer/varblocks/api/BlockEntry.java
@@ -14,14 +14,16 @@ public class BlockEntry {
     public final int y;
     public final int z;
     public final String type;
+    public final String template;
     public final Map<String, String> args;
 
-    public BlockEntry(String world, int x, int y, int z, String type, Map<String, String> args) {
+    public BlockEntry(String world, int x, int y, int z, String type, String template, Map<String, String> args) {
         this.world = world;
         this.x = x;
         this.y = y;
         this.z = z;
         this.type = type;
+        this.template = template;
         this.args = args;
     }
 
@@ -32,25 +34,34 @@ public class BlockEntry {
                 location.getBlockY(),
                 location.getBlockZ(),
                 type,
+                template,
                 args
         );
     }
 
     public BlockEntry withType(String type) {
-        return new BlockEntry(world, x, y, z, type, args);
+        return new BlockEntry(world, x, y, z, type, template, args);
+    }
+
+    public BlockEntry withTemplate(String template) {
+        return new BlockEntry(world, x, y, z, type, template, args);
     }
 
     public BlockEntry withArgs(Map<String, String> args) {
-        return new BlockEntry(world, x, y, z, type, args);
+        return new BlockEntry(world, x, y, z, type, template, args);
     }
 
     public BlockEntry withArg(String key, String value) {
         Map<String, String> newArgs = new LinkedHashMap<>(args);
         newArgs.put(key, value);
-        return new BlockEntry(world, x, y, z, type, newArgs);
+        return new BlockEntry(world, x, y, z, type, template, newArgs);
     }
 
-    public Optional<Location> location() {
-        return Optional.ofNullable(Bukkit.getWorld(world)).map(world -> new Location(world, x, y, z));
+    public boolean valid() {
+        return Bukkit.getWorld(world) != null;
+    }
+
+    public Location location() {
+        return new Location(Bukkit.getWorld(world), x, y, z);
     }
 }
