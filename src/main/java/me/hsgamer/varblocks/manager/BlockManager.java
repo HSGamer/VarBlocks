59f0d6a85c330030198a79c801e34ccf6360bb68
hsgamer <huynhqtienvtag@gmail.com>
2024-10-20 00:50:02 +0700

    assign name to BlockEntry

--- src/main/java/me/hsgamer/varblocks/manager/BlockManager.java
+++ src/main/java/me/hsgamer/varblocks/manager/BlockManager.java
@@ -3,15 +3,18 @@ package me.hsgamer.varblocks.manager;
 import io.github.projectunified.minelib.plugin.base.Loadable;
 import me.hsgamer.hscore.bukkit.config.BukkitConfig;
 import me.hsgamer.hscore.common.MapUtils;
+import me.hsgamer.hscore.config.PathString;
 import me.hsgamer.varblocks.api.BlockEntry;
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class BlockManager implements Loadable {
     private final BukkitConfig config;
-    private final Map<Location, BlockEntry> blockEntryMap = new LinkedHashMap<>();
+    private final Map<String, BlockEntry> blockEntryMap = new LinkedHashMap<>();
+    private final AtomicReference<Map<Location, BlockEntry>> blockEntryCache = new AtomicReference<>(Collections.emptyMap());
 
     public BlockManager(Plugin plugin) {
         this.config = new BukkitConfig(plugin, "blocks.yml");
@@ -22,46 +25,59 @@ public class BlockManager implements Loadable {
         return new ArrayList<>(blockEntryMap.values());
     }
 
+    public Optional<BlockEntry> getBlockEntry(String name) {
+        return Optional.ofNullable(blockEntryMap.get(name));
+    }
+
     public Optional<BlockEntry> getBlockEntry(Location location) {
-        return Optional.ofNullable(blockEntryMap.get(location));
+        return Optional.ofNullable(blockEntryCache.get().get(location));
     }
 
-    public void addBlockEntry(BlockEntry blockEntry) {
-        blockEntryMap.put(blockEntry.location(), blockEntry);
+    public void addBlockEntry(String name, BlockEntry blockEntry) {
+        blockEntryMap.put(name, blockEntry);
         saveConfig();
+        updateCache();
     }
 
-    public void removeBlockEntry(Location location) {
-        blockEntryMap.remove(location);
+    public void removeBlockEntry(String name) {
+        blockEntryMap.remove(name);
         saveConfig();
+        updateCache();
+    }
+
+    private void updateCache() {
+        Map<Location, BlockEntry> cache = new HashMap<>();
+        blockEntryMap.forEach((name, blockEntry) -> cache.put(blockEntry.location(), blockEntry));
+        blockEntryCache.set(cache);
     }
 
     private void loadConfig() {
         blockEntryMap.clear();
-        Object configObject = config.getNormalized("blocks");
-        if (configObject instanceof List) {
-            List<?> list = (List<?>) configObject;
-            for (Object o : list) {
-                Optional<Map<String, Object>> mapOptional = MapUtils.castOptionalStringObjectMap(o);
-                mapOptional.ifPresent(map -> addBlockEntry(BlockEntry.fromMap(map)));
-            }
-        }
+        Map<String[], Object> map = config.getNormalizedValues(false);
+        map.forEach((key, value) ->
+                MapUtils.castOptionalStringObjectMap(value)
+                        .map(BlockEntry::fromMap)
+                        .ifPresent(blockEntry -> blockEntryMap.put(PathString.joinDefault(key), blockEntry))
+        );
     }
 
     private void saveConfig() {
-        List<Map<String, Object>> list = new ArrayList<>();
-        blockEntryMap.values().forEach(blockEntry -> list.add(blockEntry.toMap()));
-        config.set(list, "blocks");
+        config.clear();
+        for (Map.Entry<String, BlockEntry> entry : blockEntryMap.entrySet()) {
+            config.set(entry.getValue().toMap(), entry.getKey());
+        }
         config.save();
     }
 
     @Override
     public void enable() {
         loadConfig();
+        updateCache();
     }
 
     @Override
     public void disable() {
         saveConfig();
+        blockEntryCache.set(Collections.emptyMap());
     }
 }
