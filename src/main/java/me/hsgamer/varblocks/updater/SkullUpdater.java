cb4e369a9c4c0f2f394518266b43ba5d87722616
hsgamer <huynhqtienvtag@gmail.com>
2024-10-20 17:50:58 +0700

    use XSeries to set skull

--- src/main/java/me/hsgamer/varblocks/updater/SkullUpdater.java
+++ src/main/java/me/hsgamer/varblocks/updater/SkullUpdater.java
@@ -1,80 +1,33 @@
 package me.hsgamer.varblocks.updater;
 
-import me.hsgamer.hscore.common.Validate;
+import com.cryptomorin.xseries.profiles.objects.ProfileContainer;
+import com.cryptomorin.xseries.profiles.objects.Profileable;
 import me.hsgamer.varblocks.api.BlockUpdater;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Skull;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.regex.Pattern;
 
 public class SkullUpdater implements BlockUpdater {
-    private static final Pattern playerNamePattern = Pattern.compile("[a-zA-Z0-9_]{1,16}");
-    private static final UUID skullUUID = UUID.fromString("832b9c2d-f6c2-4c5f-8e0d-e7e7c4e9f9c8");
-    private static Method setOwningPlayerMethod;
-    private static Method setOwnerMethod;
-
-    static {
-        try {
-            setOwnerMethod = Skull.class.getDeclaredMethod("setOwner", String.class);
-            setOwningPlayerMethod = Skull.class.getDeclaredMethod("setOwningPlayer", OfflinePlayer.class);
-        } catch (NoSuchMethodException e) {
-            // IGNORED
-        }
-    }
-
-    private void setOwner(Skull skull, OfflinePlayer owner) {
-        if (setOwningPlayerMethod != null) {
-            try {
-                setOwningPlayerMethod.invoke(skull, owner);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                JavaPlugin.getProvidingPlugin(getClass()).getLogger().log(Level.WARNING, "Error when setting owner for skulls", e);
-            }
-        } else {
-            try {
-                setOwnerMethod.invoke(skull, owner == null ? null : owner.getName());
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                JavaPlugin.getProvidingPlugin(getClass()).getLogger().log(Level.WARNING, "Error when setting owner for skulls", e);
-            }
-        }
-    }
-
+    @SuppressWarnings("UnstableApiUsage")
     @Override
     public void updateBlock(Block block, List<String> args) {
+        if (args.isEmpty()) return;
+
         BlockState blockState = block.getState();
         if (!(blockState instanceof Skull)) return;
         Skull skull = (Skull) blockState;
+        ProfileContainer.BlockStateProfileContainer profileContainer = new ProfileContainer.BlockStateProfileContainer(skull);
 
-        UUID uuid;
-        if (args.isEmpty()) {
-            uuid = skullUUID;
-        } else {
-            String arg = args.get(0);
-            boolean isPlayerName = args.size() > 1 && Boolean.parseBoolean(args.get(1));
-            if (isPlayerName) {
-                //noinspection deprecation
-                uuid = Optional.of(arg)
-                        .filter(s -> playerNamePattern.matcher(s).matches())
-                        .map(Bukkit::getOfflinePlayer)
-                        .filter(OfflinePlayer::hasPlayedBefore)
-                        .map(OfflinePlayer::getUniqueId)
-                        .orElse(skullUUID);
-            } else {
-                uuid = Validate.getUUID(arg).orElse(skullUUID);
-            }
+        Profileable profileable;
+        try {
+            profileable = Profileable.detect(args.get(0));
+            profileContainer.setProfile(profileable.getProfile());
+        } catch (IllegalArgumentException e) {
+            return;
         }
-        OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
 
-        setOwner(skull, player);
-        skull.update(false, false);
+        skull.update(true, false);
     }
 }
