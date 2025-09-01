da8565b78022ae232d194c9c48678047e905adb2
hsgamer <huynhqtienvtag@gmail.com>
2025-09-01 09:18:19 +0700

    suppress skull errors

--- src/main/java/me/hsgamer/varblocks/updater/SkullUpdater.java
+++ src/main/java/me/hsgamer/varblocks/updater/SkullUpdater.java
@@ -1,14 +1,15 @@
 package me.hsgamer.varblocks.updater;
 
-import com.cryptomorin.xseries.profiles.exceptions.InvalidProfileException;
 import com.cryptomorin.xseries.profiles.objects.ProfileContainer;
 import com.cryptomorin.xseries.profiles.objects.Profileable;
 import me.hsgamer.varblocks.api.BlockUpdater;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Skull;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
+import java.util.logging.Level;
 
 public class SkullUpdater implements BlockUpdater {
     @SuppressWarnings("UnstableApiUsage")
@@ -21,11 +22,16 @@ public class SkullUpdater implements BlockUpdater {
         Skull skull = (Skull) blockState;
         ProfileContainer.BlockStateProfileContainer profileContainer = new ProfileContainer.BlockStateProfileContainer(skull);
 
+        boolean showErrors = args.size() > 1 && args.get(1).equalsIgnoreCase("true");
+
         Profileable profileable;
         try {
             profileable = Profileable.detect(args.get(0));
             profileContainer.setProfile(profileable.getProfile());
-        } catch (InvalidProfileException e) {
+        } catch (Throwable e) {
+            if (showErrors) {
+                JavaPlugin.getProvidingPlugin(getClass()).getLogger().log(Level.WARNING, "Error while trying to update skull profile", e);
+            }
             return;
         }
 
