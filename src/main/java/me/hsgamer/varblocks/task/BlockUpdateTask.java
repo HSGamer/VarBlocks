174e5ff6160cd2235a55487b5360140c6c012a8e
hsgamer <huynhqtienvtag@gmail.com>
2026-07-01 11:25:03 +0700

    optimize the update task
    - reduce to one timer task
    - update block in bulk

--- src/main/java/me/hsgamer/varblocks/task/BlockUpdateTask.java
+++ src/main/java/me/hsgamer/varblocks/task/BlockUpdateTask.java
@@ -16,7 +16,7 @@ import org.bukkit.block.Block;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 
@@ -24,77 +24,75 @@ public class BlockUpdateTask implements Loadable {
     private final VarBlocks plugin;
     private final Queue<BlockEntry> updateQueue = new LinkedList<>();
     private final Queue<Map.Entry<Location, Consumer<Block>>> setBlockQueue = new ConcurrentLinkedQueue<>();
-    private final AtomicBoolean blockSetting = new AtomicBoolean(false);
-    private Task updateTask;
-    private Task setBlockTask;
+    private final AtomicInteger pendingBlockSets = new AtomicInteger(0);
+    private Task task;
 
     public BlockUpdateTask(VarBlocks plugin) {
         this.plugin = plugin;
     }
 
-    private void onUpdate() {
+    private void onTick() {
         BlockEntry entry = updateQueue.poll();
         if (entry == null) {
             updateQueue.addAll(plugin.get(BlockManager.class).getBlockEntries());
-            return;
-        }
+        } else if (entry.valid()) {
+            Location location = entry.location();
 
-        if (!entry.valid()) return;
-        Location location = entry.location();
+            Optional<BlockUpdater> updaterOptional = plugin.get(BlockUpdaterManager.class).getBlockUpdater(entry.type);
+            if (updaterOptional.isPresent()) {
+                BlockUpdater updater = updaterOptional.get();
+                List<String> args = plugin.get(TemplateManager.class).getParsedTemplate(entry.template, entry.args);
 
-        Optional<BlockUpdater> updaterOptional = plugin.get(BlockUpdaterManager.class).getBlockUpdater(entry.type);
-        if (!updaterOptional.isPresent()) return;
-        BlockUpdater updater = updaterOptional.get();
-
-        List<String> args = plugin.get(TemplateManager.class).getParsedTemplate(entry.template, entry.args);
-
-        Consumer<Block> consumer = updater.getUpdateTask(args);
-        if (consumer != null) {
-            setBlockQueue.add(new AbstractMap.SimpleEntry<>(location, consumer));
-        }
-    }
-
-    private void onBlockSet() {
-        if (blockSetting.get()) return;
-
-        Map.Entry<Location, Consumer<Block>> entry = setBlockQueue.poll();
-        if (entry == null) return;
-
-        Location location = entry.getKey();
-
-        blockSetting.set(true);
-        LocationScheduler.get(plugin, location).run(() -> {
-            try {
-                World world = location.getWorld();
-                assert world != null;
-                int chunkX = location.getBlockX() >> 4;
-                int chunkZ = location.getBlockZ() >> 4;
-                if (world.isChunkLoaded(chunkX, chunkZ)) {
-                    entry.getValue().accept(location.getBlock());
+                Consumer<Block> consumer = updater.getUpdateTask(args);
+                if (consumer != null) {
+                    setBlockQueue.add(new AbstractMap.SimpleEntry<>(location, consumer));
                 }
-            } catch (Throwable throwable) {
-                plugin.getLogger().log(Level.WARNING, "Error while updating block at " + location, throwable);
-            } finally {
-                blockSetting.set(false);
             }
-        });
+        }
+
+        if (pendingBlockSets.get() > 0) return;
+
+        Map<Location, Consumer<Block>> setBlockMap = new HashMap<>();
+        {
+            Map.Entry<Location, Consumer<Block>> blockEntry;
+            while ((blockEntry = setBlockQueue.poll()) != null) {
+                setBlockMap.merge(blockEntry.getKey(), blockEntry.getValue(), Consumer::andThen);
+            }
+        }
+
+        for (Map.Entry<Location, Consumer<Block>> blockEntry : setBlockMap.entrySet()) {
+            Location location = blockEntry.getKey();
+            World world = location.getWorld();
+            if (world == null) continue;
+            int chunkX = location.getBlockX() >> 4;
+            int chunkZ = location.getBlockZ() >> 4;
+            if (!world.isChunkLoaded(chunkX, chunkZ)) continue;
+
+            pendingBlockSets.incrementAndGet();
+            Consumer<Block> consumer = blockEntry.getValue();
+            LocationScheduler.get(plugin, location).run(() -> {
+                try {
+                    consumer.accept(location.getBlock());
+                } catch (Throwable throwable) {
+                    plugin.getLogger().log(Level.WARNING, "Error while updating block at " + location, throwable);
+                } finally {
+                    pendingBlockSets.decrementAndGet();
+                }
+            });
+        }
     }
 
     @Override
     public void enable() {
-        updateTask = AsyncScheduler.get(plugin).runTimer(this::onUpdate, 0, 0);
-        setBlockTask = AsyncScheduler.get(plugin).runTimer(this::onBlockSet, 0, 0);
+        task = AsyncScheduler.get(plugin).runTimer(this::onTick, 0, 0);
     }
 
     @Override
     public void disable() {
-        if (updateTask != null) {
-            updateTask.cancel();
-            updateTask = null;
-        }
-        if (setBlockTask != null) {
-            setBlockTask.cancel();
-            setBlockTask = null;
+        if (task != null) {
+            task.cancel();
+            task = null;
         }
     }
 }
+
